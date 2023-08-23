8b2156a295ecf4c416a32f8632660e78c8a87e1a
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-08-23 21:32:52 +0100

    News Home article

--- src/components/Spotlight/spotlight.jsx
+++ src/components/Spotlight/spotlight.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { ModelDiamond as Icon } from "react-basicons";
+import { StyledContainer} from './spotlight.style';
 
 function Spotlight() {
     return (
-        <div>
-        NEWS
-        </div>
+        <StyledContainer>
+            <div className="spotlight-section">
+                <div className="title">NEWS</div>
+                <p className= "description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, magnam aut. Unde eos ipsam quidem id officia quisquam pariatur facere esse perferendis repellat, repellendus placeat, porro omnis hic ex corrupti.</p>
+                <div className="dividers-container">
+                    <div className="divider" />
+                    <div className="icon-container"><Icon/></div>
+                    <div className="divider" />
+                </div>              
+            </div> 
+        </StyledContainer>
     );
 }
 
