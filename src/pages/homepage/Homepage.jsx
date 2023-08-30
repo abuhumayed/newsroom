51b392686a97f245967ea98b1f03b81c8c28f30b
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-08-30 14:21:22 +0100

    Revert "Update Homepage.jsx"
    This reverts commit 1ec11e0680010528061abc39c6eaf5bb1969ba12.

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import { Spotlight } from '../../components/Spotlight'
 import { ArticleCard } from '../../components/ArticleCard'
 import { StyledContainer } from './homepage.style'
-import articles  from '../../data/articles.json'
-import quick  from '../../data/quick.json'
-import { QuickInfoCard } from '../../components/QuickInfoCard'
 
 
 function Homepage(props) {
@@ -15,27 +12,7 @@ function Homepage(props) {
         
         <div className="news-container">
           <div className="articles-section">
-            {articles.map(({urlToImage,title,description}) => (
-              <ArticleCard
-                img={urlToImage}
-                title={title}
-                description={description}
-              />
-            
-            )) }
-         
-  
-           
-          </div>
-          <div className="quick-info-section">
-            {quick.map(({title,description}) => (
-              <QuickInfoCard
-                title={title}
-                description={description}
-                
-
-/>            
-            ))}
+            <ArticleCard />
           </div>
         </div>
       </div>
