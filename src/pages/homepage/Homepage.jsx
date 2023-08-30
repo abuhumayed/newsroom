1ec11e0680010528061abc39c6eaf5bb1969ba12
abuhumayed <ahmedthanjim@hotmail.co.uk>
2023-08-30 14:00:50 +0100

    Update Homepage.jsx

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Spotlight } from '../../components/Spotlight'
 import { ArticleCard } from '../../components/ArticleCard'
 import { StyledContainer } from './homepage.style'
+import articles  from '../../data/articles.json'
+import quick  from '../../data/quick.json'
+import { QuickInfoCard } from '../../components/QuickInfoCard'
 
 
 function Homepage(props) {
@@ -12,7 +15,27 @@ function Homepage(props) {
         
         <div className="news-container">
           <div className="articles-section">
-            <ArticleCard />
+            {articles.map(({urlToImage,title,description}) => (
+              <ArticleCard
+                img={urlToImage}
+                title={title}
+                description={description}
+              />
+            
+            )) }
+         
+  
+           
+          </div>
+          <div className="quick-info-section">
+            {quick.map(({title,description}) => (
+              <QuickInfoCard
+                title={title}
+                description={description}
+                
+
+/>            
+            ))}
           </div>
         </div>
       </div>
